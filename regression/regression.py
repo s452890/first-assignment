058477b8407723eea4ebb76ed7fb43844ea6ed0c
Radosław Jaszczak <radjas@st.amu.edu.pl>
2020-05-30 13:34:24 +0200

    removed outliers with quantile method

--- regression/regression.py
+++ regression/regression.py
@@ -4,12 +4,18 @@ import numpy as np
 import seaborn as sns
 
 df_survey = pd.read_csv(r'.\survey_results_public.csv',
-                        usecols=['Respondent', 'YearsCode', 'Age1stCode', 'WorkWeekHrs',
-                                 'ConvertedComp', 'Age', 'JobSat', 'Student', 'Country'],
+                        usecols=['Respondent', 'YearsCode', 'Age1stCode', 'WorkWeekHrs', 'Student',
+                                 'ConvertedComp', 'Age', 'JobSat', 'Country'],
                         index_col=['Respondent'])
 df_survey['Country'] = df_survey.Country.apply(lambda x: 0 if str(x).find("United States") == -1 else 1)
 df_survey.rename(columns={'Country':'from_USA'}, inplace=True)
 # deal with string values:
+df_survey['JobSat'] = df_survey['JobSat'].map({'Slightly satisfied' : 'Satisfied',
+                                               'Slightly dissatisfied' : 'Dissatisfied',
+                                               'Neither satisfied nor dissatisfied' : 'Not specified',
+                                               'Very satisfied' : 'Satisfied',
+                                               'Very dissatisfied': 'Dissatisfied'})
+
 df_survey['JobSat'] = df_survey['JobSat'].astype('category')
 df_survey['JobSatCat'] = df_survey['JobSat'].cat.codes
 sat_dictionary = dict(enumerate(df_survey.JobSat.cat.categories)) # save infromation for later
@@ -17,14 +23,14 @@ df_survey = df_survey.drop('JobSat', 1)
 df_survey = pd.get_dummies(df_survey, columns=['JobSatCat']) # one hot encode
 df_survey['Student'] = df_survey['Student'].map({'Yes, part-time' : 1, 'Yes, full-time' : 1, 'No' : 0})
 df_survey.dropna(inplace=True)
-df_survey.YearsCode = pd.to_numeric(df_survey.YearsCode, errors='coerce')
-df_survey.Age1stCode = pd.to_numeric(df_survey.Age1stCode, errors='coerce')
-df_survey.WorkWeekHrs = pd.to_numeric(df_survey.WorkWeekHrs, errors='coerce')
-# remove outliers:
-outliers_columns = ['ConvertedComp', 'WorkWeekHrs', 'Age', 'Age1stCode']
-for col in outliers_columns:
-    df_survey = df_survey[np.abs(df_survey[col] - df_survey[col].mean()) <= 1*df_survey[col].std()]
-
+for column in ['YearsCode', 'Age1stCode', 'WorkWeekHrs']:
+    df_survey[column] = pd.to_numeric(df_survey[column], errors='coerce')
+# remove outliers with quantile method:
+Q1 = df_survey[['ConvertedComp', 'WorkWeekHrs']].quantile(0.25)
+Q3 = df_survey[['ConvertedComp', 'WorkWeekHrs']].quantile(0.75)
+IQR = Q3 - Q1
+df_survey = df_survey[~((df_survey[['ConvertedComp', 'WorkWeekHrs']] < (Q1 - 1.5 * IQR)) 
+                        | (df_survey[['ConvertedComp', 'WorkWeekHrs']] > (Q3 + 1.5 * IQR))).any(axis=1)]
 # plot correlation matrix
 corr = df_survey.corr()
 fig = plt.figure()
